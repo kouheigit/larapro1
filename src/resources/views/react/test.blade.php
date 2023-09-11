d632890839b7f6df5dc6d27d95423c3270851237
kouheigit <ookawakouhei777@gmail.com>
2023-09-11 23:22:53 +0900

    React dev4

--- src/resources/views/react/test.blade.php
+++ src/resources/views/react/test.blade.php
@@ -1,6 +1,6 @@
 <meta name="csrf-token" content="{{ csrf_token() }}">
 <link href="{{ mix('css/app.css') }}" rel="stylesheet" type="text/css">
 <link href="{{ mix('css/app.css') }}" rel="stylesheet" type="text/css">
-<div id="example"></div>
+<div id="test"></div>
 <script src="{{ mix('js/app.js') }}"></script>
 <h1>ナメックムーブ</h1>
