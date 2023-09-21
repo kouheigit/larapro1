3141cf7191cabf7ccf98bea1445b680124ce1d80
kouheigit <ookawakouhei777@gmail.com>
2023-09-21 16:12:02 +0900

    React dev43

--- src/resources/views/react/componentpage.blade.php
+++ src/resources/views/react/componentpage.blade.php
@@ -1,2 +1,6 @@
-<h1>React-component</h1>
+<meta name="csrf-token" content="{{ csrf_token() }}">
+<link href="{{ mix('css/app.css') }}" rel="stylesheet" type="text/css">
+<link href="{{ mix('css/app.css') }}" rel="stylesheet" type="text/css">
+<h1>React</h1>
 <div id="Compindex"></div>
+<script src="{{ mix('js/app.js') }}"></script>
