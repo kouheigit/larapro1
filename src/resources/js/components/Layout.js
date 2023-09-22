da8c1776de92ac60dcfe46fad0d3f85f1c799657
kouheigit <ookawakouhei777@gmail.com>
2023-09-22 13:22:33 +0900

    React dev51

---
+++ src/resources/js/components/Layout.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom';
+import Header from '../components/Compheader'
+import Footer from '../components/Compfooter'
+
+export default function Layout({ children }){
+    return (
+        <>
+            <Header />
+
+               <main>{children}</main>
+
+            <Footer />
+        </>
+    )
+}
