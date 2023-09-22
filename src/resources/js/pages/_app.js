6ab1c55f6e82cfbcc39d928303d27e95b4e026c3
kouheigit <ookawakouhei777@gmail.com>
2023-09-22 18:06:30 +0900

    React dev58

--- src/resources/js/pages/_app.js
+++ src/resources/js/pages/_app.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom';
-import Layout from "../components/layout";
 
+import Layout from "../components/layout";
+//MyApp
 function MyApp({ Component, pageProps }){
     return (
             <Layout>
