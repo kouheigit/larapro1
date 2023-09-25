dcbadb7e216e53acbcd7e6c6725b5b410f6cc0f6
kouheigit <ookawakouhei777@gmail.com>
2023-09-25 19:40:50 +0900

    React dev70

--- src/resources/js/blog/index.js
+++ src/resources/js/blog/index.js
@@ -1,5 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom';
 import Layout from "../components/layout";
+import Example from "../components/Example";
+
+export default function Hero(){
+    return (
+       <div id="herotest">
+           <h1>CUBE</h1>
+           <p>アウトプットしていくサイト</p>
+       </div>
+    )
+}
 
 
+
+if (document.getElementById('herotest')) {
+    ReactDOM.render(<herotest />, document.getElementById('herotest'));
+}
