6e3a45f625bcec8bd790557e8b06dd219a30c173
kouheigit <ookawakouhei777@gmail.com>
2023-12-12 13:21:01 +0900

    React dev95

--- src/resources/js/app.js
+++ src/resources/js/app.js
@@ -12,7 +12,6 @@ require('./bootstrap');
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 //React-Test.blade.php
-//React-Test.blade.php
 require('./components/Test');
 require('./components/Test1');
 require('./components/Test2');
