1e2273d0d8e8da4c330139f772266164723bb64b
kouheigit <ookawakouhei777@gmail.com>
2023-12-14 15:18:45 +0900

    React dev98

--- src/resources/views/react/blogabout.blade.php
+++ src/resources/views/react/blogabout.blade.php
@@ -3,4 +3,5 @@
 <link href="{{ mix('css/app.css') }}" rel="stylesheet" type="text/css">
 <h1>なめっくムーブ</h1>
 <div id="blogabout"></div>
+<div id="bloghome"></div>
 <script src="{{ mix('js/app.js') }}"></script>
