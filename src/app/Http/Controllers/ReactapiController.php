5a0497ecd628e2a809af921492658115f8815020
kouheigit <ookawakouhei777@gmail.com>
2023-12-15 15:51:39 +0900

    End for setting to controller for the API

--- src/app/Http/Controllers/ReactapiController.php
+++ src/app/Http/Controllers/ReactapiController.php
@@ -8,10 +8,12 @@ class ReactapiController extends Controller
 {
     public function insert(Request $request)
     {
+        /*
         $insert = new Insert();
         $insert->name = $request->name;
         $insert->password = $request->password;
         $insert->save();
-        return response()->json($insert,200);
+        return response()->json($insert,200);*/
+        return response()->json(200);
     }
 }
--- src/routes/api.php
+++ src/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ReactapiController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,7 +18,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-
 Route::group(['middleware' => 'api'], function () {
-    Route::post('Reactapi', 'App\Http\Controllers\ReactapiController@insert');
+    Route::match(['post', 'get'], 'Reactapi', [ReactapiController::class, 'insert']);
 });
