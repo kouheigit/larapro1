57d26d8080a670ed74b112014758f65da456e47c
kouheigit <ookawakouhei777@gmail.com>
2024-05-05 22:18:37 +0900

    dev21

--- src/reacttest/src/Texttest.tsx
+++ src/reacttest/src/Texttest.tsx
@@ -7,6 +7,7 @@ export default function Texttest() {
             <h1>Value test</h1>
             <h1>どうする徳川家康</h1>
             <h2>どうしない家康</h2>
+            <h1>億万長者</h1>
             <p>宮本天外</p>
             <p>億万長者</p>
             <p>複数行記述する時は中に何も記述してないカッコを記述する</p>
