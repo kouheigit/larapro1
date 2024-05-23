a5b6414ee7af8572613e3412fe6b640da83b955d
kouheigit <ookawakouhei777@gmail.com>
2024-05-23 19:17:11 +0900

    pokemondev52

--- src/pokemon/src/App.js
+++ src/pokemon/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 //STEP5 useStateで状態管理からスタート
 //useEffect,useStateを追加している
+//pokemon
 import { useEffect, useState} from "react";
 import PokemonThumbnails from "./PokemonThumbnails";
 import pokemonJson from "./pokemon.json";
