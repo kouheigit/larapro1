121c6a7877a49b39b3cae9467e9dc8479331b95d
kouheigit <ookawakouhei777@gmail.com>
2024-05-23 16:14:10 +0900

    pokemondev49

--- src/pokemon/src/PokemonThumbnails.jsx
+++ src/pokemon/src/PokemonThumbnails.jsx
@@ -8,7 +8,6 @@ const PokemonThumbnails = ({id,name,image,type}) => {
                 <div className="number">
                     <small>#0{id}</small>
                 </div>
-                <img src={image} alt={name}/>
                 <img src={iconImage} alt={name} className="icon-image" />
                 <div className="detail-wrapper">
                     <h4>{name}</h4>
