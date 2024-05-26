3001cd3305353cd297b85ba382557eae3d34f4ec
kouheigit <ookawakouhei777@gmail.com>
2024-05-27 03:13:56 +0900

    pokemondev12

--- src/pokemon1/src/PokemonThumbnails.jsx
+++ src/pokemon1/src/PokemonThumbnails.jsx
@@ -1,22 +1,19 @@
 import React from "react";
-
-//コンポーネントを定義する
-/*
-+const PokemonThumbnails = ({ id, name, image, type }) => {
+const PokemonThumbnails =({ id,name,image,type }) => {
     return (
         <div>
-            +     <div className="number">
-            +        <small>#0{id}</small>
-            +      </div>
-            +      <img src={image} alt={name} />
-            +      <div className="detail-wrapper">
-            +        <h4>{name}</h4>
-            +        <h3>{type}</h3>
-            +      </div>
+            <div className="number">
+                <small>#0{id}</small>
+            </div>
+            <img src={image} alt={name} />
+            <div className="detail-wrapper">
+                <h4>{name}</h4>
+                <h3>{type}</h3>
+            </div>
         </div>
     );
-};
-*/
+}
+
 //デフォルトエクスポートする
 export default PokemonThumbnails;
 
