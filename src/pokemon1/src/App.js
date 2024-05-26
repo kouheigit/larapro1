012be54c691aec75b42830c27e47332bfa85278a
kouheigit <ookawakouhei777@gmail.com>
2024-05-27 02:42:17 +0900

    pokemondev9

--- src/pokemon1/src/App.js
+++ src/pokemon1/src/App.js
@@ -1,10 +1,45 @@
 import './App.css';
 import PokemonThumbnails from './PokemonThumbnails';
 
+/*
 function App() {
++  // 仮でデータを作成する
++  const pokemon = {
++    id: 1,
++    name: "フシギダネ",
++    image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
++    type: "くさ"
++  }
+
   return (
     <div className="App">
-        <PokemonThumbnails/>
+      <PokemonThumbnails
++       id={pokemon.id}
++       name={pokemon.name}
++       image={pokemon.image}
++       type={pokemon.type}
+       />
+    </div>
+  );
+}
+export default App;
+ */
+function App() {
+    const pokemon ={
+        id: 1,
+        name: "フシギダネ",
+        image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+        tyep:"くさ"
+    }
+
+  return (
+    <div className="App">
+        <PokemonThumbnails
+        id={pokemon.id}
+        name={pokemon.name}
+        image={pokemon.image}
+        tyep={pokemon.type}
+        />
     </div>
   );
 }
