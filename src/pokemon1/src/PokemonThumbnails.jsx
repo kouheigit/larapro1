3610f20fa40e241f31169971cf6e32e8d8d8d604
kouheigit <ookawakouhei777@gmail.com>
2024-05-27 01:35:27 +0900

    pokemondev6

---
+++ src/pokemon1/src/PokemonThumbnails.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+//コンポーネントを定義する
+const PokemonThumbnails = () =>{
+    return<div>PokemonThimbnails</div>
+};
+
+//デフォルトエクスポートする
+export default PokemonThumbnails;
+
+/*
+import React from "react";
+
+// アロー関数でコンポーネントを定義
+const PokemonThumbnails = () => {
+  return <div>PokemonThumbnails</div>;
+};
+
+// デフォルトエクスポートする
+export default PokemonThumbnails;
+ */
