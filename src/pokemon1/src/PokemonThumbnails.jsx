1ee0e944a580c50b17f3ba140d2d1b22c966d7d3
kouheigit <ookawakouhei777@gmail.com>
2024-05-30 18:38:57 +0900

    pokemondev37

--- src/pokemon1/src/PokemonThumbnails.jsx
+++ src/pokemon1/src/PokemonThumbnails.jsx
@@ -1,27 +1,14 @@
 import React from "react";
 
-const PokemonThumbnails = ({ id ,name,image,type}) =>{
-    return (
-        <div>
-            <div className="number">
-                <small>#0{id}</small>
-            </div>
-            <img src={image} alt={name} />
-            <div className="detail-wrapper">
-                <h4>{name}</h4>
-                <h3>{type}</h3>
-            </div>
-        </div>
-    );
-};
-export default PokemonThumbnails;
-
+const PokemonThumbnails =({ id, name,image,type}) =>{
+    
+}
 /*
- const PokemonThumbnails = ({ id, name, image, type }) => {
+const PokemonThumbnails = ({ id, name, image, type }) => {
     return (
         <div>
-                  <div className="number">
-                     <small>#0{id}</small>
+                 <div className="number">
+                    <small>#0{id}</small>
                   </div>
                   <img src={image} alt={name} />
                   <div className="detail-wrapper">
@@ -31,6 +18,5 @@ export default PokemonThumbnails;
         </div>
     );
 };
+
 export default PokemonThumbnails;*/
-
-
