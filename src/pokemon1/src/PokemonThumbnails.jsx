81a2bf188579b805d6726adfbd46c6c167a59a71
kouheigit <ookawakouhei777@gmail.com>
2024-05-30 18:45:03 +0900

    pokemondev39

--- src/pokemon1/src/PokemonThumbnails.jsx
+++ src/pokemon1/src/PokemonThumbnails.jsx
@@ -15,20 +15,4 @@ const PokemonThumbnails =({ id, name,image,type}) =>{
     );
 };
 export default PokemonThumbnails;
-/*
-const PokemonThumbnails = ({ id, name, image, type }) => {
-    return (
-        <div>
-                 <div className="number">
-                    <small>#0{id}</small>
-                  </div>
-                  <img src={image} alt={name} />
-                  <div className="detail-wrapper">
-                    <h4>{name}</h4>
-                    <h3>{type}</h3>
-                  </div>
-        </div>
-    );
-};
 
-export default PokemonThumbnails;*/
