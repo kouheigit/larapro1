84a7479bb2273b9b225255e35d53b296e4b01208
kouheigit <ookawakouhei777@gmail.com>
2024-05-30 17:11:51 +0900

    pokemondev32

--- src/pokemon1/src/App.js
+++ src/pokemon1/src/App.js
@@ -1,8 +1,9 @@
 import './App.css';
-
+import PokemonThumbnails from "./PokemonThumbnails";
 function App() {
   return (
     <div className="App">
+        <PokemonThumbnails />
     </div>
   );
 }
