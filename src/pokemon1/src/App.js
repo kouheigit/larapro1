aa71a414fa6cb6f79eceb178b936bf44e9c72b8c
kouheigit <ookawakouhei777@gmail.com>
2024-05-31 19:23:03 +0900

    pokemondev51

--- src/pokemon1/src/App.js
+++ src/pokemon1/src/App.js
@@ -24,16 +24,6 @@ function App() {
             type: "くさ",
         }
     ]
-    /*
-    const url ="https://pokeapi.co/api/v2/pokemon";
-    useEffect(() => {
-        fetch(url)
-            .then(response =>response.json)
-            .then(data => {
-                console.log(data);
-            })
-    },[])*/
-
     const url ="https://pokeapi.co/api/v2/pokemon";
     useEffect(()=> {
         fetch(url)
