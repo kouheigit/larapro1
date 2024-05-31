35720a0830eb65d5449e6f64ddf11c6ba3a3d134
kouheigit <ookawakouhei777@gmail.com>
2024-05-31 19:13:52 +0900

    pikemondev49

--- src/pokemon1/src/PokemonThumbnails.jsx
+++ src/pokemon1/src/PokemonThumbnails.jsx
@@ -3,8 +3,15 @@ import React from "react";
 
 const PokemonThumbnails =({ id, name,image,type}) =>{
     return (
-        <div>
-     
+        <div className="thumb-container grass">
+            <div className="number">
+                <small>#0{id}</small>
+            </div>
+            <img src={image} alt={name} />
+            <div className="detail-wrapper">
+                <h4>{name}</h4>
+                <h3>{type}</h3>
+            </div>
         </div>
     );
 };
