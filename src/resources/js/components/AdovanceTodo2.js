80eb244f7f5ec05df44025d41bef58cbc6bbd888
kouheigit <ookawakouhei777@gmail.com>
2025-04-21 13:21:02 +0900

    dev54

--- src/resources/js/components/AdovanceTodo2.js
+++ src/resources/js/components/AdovanceTodo2.js
@@ -5,7 +5,11 @@ function AdvanceTodo2() {
     const { todos,setTodos,inputs,setInput, addTodo, deleteTodo, toggleCheck } = useCounter();
     const[filter,setFilter] = useState('all');
 
-    
+    const filteredTodos = todos.filter((todo)=>{
+       if(filter === 'done') return todo.done;
+       if(filter === 'undone') return !todo.done;
+       return true;
+    });
 
     return (
         <div className="todo">
