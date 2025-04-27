d2f67f4bb4ea012762bf8f856815b09e0a1ab076
kouheigit <ookawakouhei777@gmail.com>
2025-04-27 10:10:41 +0900

    dev2

---
+++ src/resources/js/components/AdvanceTodo4.js
@@ -0,0 +1,13 @@
+import ReactDOM from "react-dom";
+import React,{ useState } from "react";
+
+
+function AdvanceTodo4(){
+    
+}
+
+export default AdvanceTodo4;
+
+if (document.getElementById('AdvanceTodo4')) {
+    ReactDOM.render(<AdvanceTodo4 />, document.getElementById('AdvanceTodo4'));
+}
