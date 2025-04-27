c73b6040a9d96ad40b654d8644daf81aa75507ab
kouheigit <ookawakouhei777@gmail.com>
2025-04-27 21:05:54 +0900

    dev14

--- src/resources/js/components/AdovanceTodo4.js
+++ src/resources/js/components/AdovanceTodo4.js
@@ -14,7 +14,17 @@ function AdovanceTodo4(){
     const deleteTodo = (deleteIndex)=>{
         setTodos(todos.filter((test,index)=>index!==deleteIndex))
     }
-  
+    const toggleCheck = (index) => {
+        setTodos(
+            todos.map((todo, i) => {
+                if (i === index) {
+                    return { ...todo, done: !todo.done };
+                } else {
+                    return todo;
+                }
+            })
+        );
+    };
     /*
     const filter_include_a = members.filter((output, index) => {
   return output.includes("a");
