36045a2adcc1e762a2d3cc2116d520c6196b64b6
kouheigit <ookawakouhei777@gmail.com>
2025-04-28 16:57:23 +0900

    dev33

--- src/resources/js/components/AdovanceTodo4.js
+++ src/resources/js/components/AdovanceTodo4.js
@@ -30,7 +30,6 @@ function AdovanceTodo4(){
             <button onClick={addTodo}>追加</button>
             {todos.map((todo,index) => (
                 <li key={index}>
-                    <input type="checkbox" onClick={()=>toggleCheck(index)} />
                     <p>{todo.text}</p>
                     <button onClick={()=>deleteTodo(index)}>削除する</button>
                 </li>
