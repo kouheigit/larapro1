77f66adc5c8b9373c72e1a87eb6e570d56b6e49f
kouheigit <ookawakouhei777@gmail.com>
2025-04-29 20:27:10 +0900

    dev48

--- src/resources/views/react/reactrouting.blade.php
+++ src/resources/views/react/reactrouting.blade.php
@@ -3,4 +3,5 @@
 <link href="{{ mix('css/app.css') }}" rel="stylesheet" type="text/css">
 <h1>React root</h1>
 <div id="AdovanceTodo4"></div>
+<div id="App"></div>
 <script src="{{ mix('js/app.js') }}"></script>
