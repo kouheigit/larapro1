2f821b8c1f01f9a3e4e64aa683b3301de3ffb7dd
kouheigit <ookawakouhei777@gmail.com>
2025-04-29 20:05:07 +0900

    dev46

---
+++ src/resources/js/components/App.js
@@ -0,0 +1,11 @@
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+import ReactDOM from 'react-dom';
+
+function App(){
+
+}
+export default App;
+
+if (document.getElementById('App')) {
+    ReactDOM.render(<App />, document.getElementById('App'));
+}
