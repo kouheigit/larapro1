e3630344c0a4bafe56073826aeef72f34607d0cd
kouheigit <ookawakouhei777@gmail.com>
2025-04-30 17:07:51 +0900

    dev70

--- src/resources/js/components/ListPage.js
+++ src/resources/js/components/ListPage.js
@@ -1,8 +1,10 @@
 import ReactDOM from 'react-dom';
 import React,{ useState } from 'react';
+import {useCounter3} from "../hooks/useCounter3";
 
 
 function ListPage(){
+    const { todos,setTodos,inputs,setInput, addTodo, deleteTodo, toggleCheck } = useCounter3();
     return(
         <div>
             <h1>テストリストページ</h1>
