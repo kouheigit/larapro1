a38f5e2924f1e0917807cb1bcb0177ac67ffec2f
kouheigit <ookawakouhei777@gmail.com>
2025-04-30 22:28:23 +0900

    dev75

--- src/resources/js/components/AdvanceTodo.js
+++ src/resources/js/components/AdvanceTodo.js
@@ -4,7 +4,8 @@ import React, { useState } from 'react';
 function AdvanceTodo(){
     const [todos,setTodos] = useState([]);
     const [inputs,setInputs] = useState('');
-    
+
+
     return(
         <div>
 
