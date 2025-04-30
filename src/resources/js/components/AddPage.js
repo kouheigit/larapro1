daaf7694dbdb1f266312cb0c50eac90a5aee9fd3
kouheigit <ookawakouhei777@gmail.com>
2025-04-30 15:27:15 +0900

    dev65

--- src/resources/js/components/AddPage.js
+++ src/resources/js/components/AddPage.js
@@ -3,6 +3,11 @@ import React,{ useState }from 'react';
 import ListPage from "./ListPage";
 
 function AddPage(){
+    return(
+        <div>
+            <h1>テスト追加ページ</h1>
+        </div>
+    );
 
 }
 
