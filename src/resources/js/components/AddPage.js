f78211ed879ac37fe4cc4fa09222a9bbe93f05c9
kouheigit <ookawakouhei777@gmail.com>
2025-04-30 14:37:08 +0900

    dev62

---
+++ src/resources/js/components/AddPage.js
@@ -0,0 +1,13 @@
+import ReactDOM from 'react-dom';
+import React,{useState }from 'react';
+import ListPage from "./ListPage";
+
+function AddPage(){
+
+}
+
+export default AddPage;
+
+if (document.getElementById('AddPage')) {
+    ReactDOM.render(<AddPage />, document.getElementById('AddPage'));
+}
