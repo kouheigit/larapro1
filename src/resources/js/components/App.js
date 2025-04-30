8f9e3969092c2c4d2b84be2d4d266ebb7abd557b
kouheigit <ookawakouhei777@gmail.com>
2025-04-30 10:33:24 +0900

    dev52

--- src/resources/js/components/App.js
+++ src/resources/js/components/App.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 
 function App(){
     return (
-        <div>
-            <h1>表示テスト</h1>
-            <h1>小林内田</h1>
-        </div>
+        <Router>
+            <nav>
+
+            </nav>
+        </Router>
     );
 }
 
