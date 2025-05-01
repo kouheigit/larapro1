1de83e917dc4b6a7c38cb20390f4d1cb8d89cffa
kouheigit <ookawakouhei777@gmail.com>
2025-05-01 09:45:21 +0900

    dev78

---
+++ src/resources/js/components/ReviewaddTodo.js
@@ -0,0 +1,18 @@
+import ReactDOM from 'react-dom';
+import React, { useState } from 'react';
+import ReviewaddTodo from "./ReviewaddTodo";
+
+function ReviewaddTodo(){
+    return(
+        <div>
+            <h1>ReviewaddTodoは正常に表示されてます</h1>
+        </div>
+    );
+
+}
+
+export default ReviewaddTodo;
+
+if (document.getElementById('ReviewaddTodo')) {
+    ReactDOM.render(<ReviewaddTodo />, document.getElementById('ReviewaddTodo'));
+}
