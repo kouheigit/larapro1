cc13ab66d0053ec889d4c9c27de5198bb97789c1
kouheigit <ookawakouhei777@gmail.com>
2025-05-04 17:22:58 +0900

    con3

--- src/resources/js/components/Context.js
+++ src/resources/js/components/Context.js
@@ -12,26 +12,15 @@ function Context(){
         return true;
     });
     return(
-        <div>
-            
+        <div class="todo">
+            <input type="text" value={inputs} onChange={(e)=>setInput(e.target.value)}/>
+            <p>入力された値:{inputs}</p>
+            <button onClick={addTodo}>追加</button>
         </div>
     );
 
 }
 /*
-import ReactDOM from 'react-dom';
-import { useCounter3 } from '../hooks/useCounter3';
-import React,{ useState } from 'react';
-
-function AdvanceTodo4() {
-    const { todos,setTodos,inputs,setInput, addTodo, deleteTodo, toggleCheck } = useCounter3();
-    const[filter,setFilter] = useState('all');
-
-    const filteredTodos = todos.filter((todo)=>{
-        if(filter === 'done') return todo.done;
-        if(filter === 'undone') return !todo.done;
-        return true;
-    });
 
     return (
         <div className="todo">
