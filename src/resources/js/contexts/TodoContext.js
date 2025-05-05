1a564563c01dbad9e1803e20bd56d343b19b460c
kouheigit <ookawakouhei777@gmail.com>
2025-05-05 09:19:02 +0900

    con12

--- src/resources/js/contexts/TodoContext.js
+++ src/resources/js/contexts/TodoContext.js
@@ -3,4 +3,47 @@ const TodoContext = createContext();
 
 export function TodoProvider ({ children }){
 
+    const [todos, setTodos] = useState([]);
+    const [inputs, setInput] = useState('');
+
+    //ローカルストレージを出力している
+    useEffect(() =>{
+        const stored = localStorage.getItem('todos');
+        if(stored){
+            setTodos(JSON.parse(stored));
+        }
+    },[])
+
+    //ストレージに保存する処理
+    useEffect(()=>{
+        localStorage.setItem('todos',JSON.stringify(todos));
+    },[todos])
+
+
+
+    //値を追加するメソット
+    const addTodo = () => {
+        if (inputs.trim() === '') return;
+        setTodos([...todos, {text: inputs, done: false}]);
+        //setTodos([...todos,inputs]);
+        setInput('');
+    };
+
+    //値を削除するメソット
+    const deleteTodo = (deleteIndex) => {
+        setTodos(todos.filter((_, index) => index !== deleteIndex));
+    };
+
+    const toggleCheck = (index) => {
+        setTodos(
+            todos.map((todo, i) => {
+                if (i === index) {
+                    return {...todo, done: !todo.done};
+                } else {
+                    return todo;
+                }
+            })
+        );
+    }
+
 }
