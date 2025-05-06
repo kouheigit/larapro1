ea1b7fd538d99beece11c392be31218053a534a7
kouheigit <ookawakouhei777@gmail.com>
2025-05-06 13:14:04 +0900

    con23

--- src/resources/js/division/TodoItem.jsx
+++ src/resources/js/division/TodoItem.jsx
@@ -1,14 +1,2 @@
-import React from "react";
 
-export default function TodoItems({ todo, index, toggleCheck, deleteTodo }){
-    return(
-        <li>
-           <input type="checkbox" checked={todo.done} onChange={()=>toggleCheck(index)}/>
-            <span style={{ textDecoration: todo.done ? 'line-through' : 'none' }}>
-            {todo.text}
-            </span>
-            <button onClick={()=>deleteTodo(index)}>削除</button>
-        </li>
-    );
-}
 
