4909738f36c3afa8999c35dde81f43dc498a40f4
kouheigit <ookawakouhei777@gmail.com>
2025-05-06 13:23:32 +0900

    con25

--- src/resources/js/division/TodoItem.jsx
+++ src/resources/js/division/TodoItem.jsx
@@ -1,3 +1,11 @@
-export default function TodoItem(){
-
+export default function TodoItem({ todo,index,toggleCheck,deleteTodo }){
+    return(
+        <li key="index">
+            <input type="checkbox" value={todo.done} onChange={()=>toggleCheck(index)}/>
+            <span style={{ textDecoration: todo.done ? 'line-through' : 'none' }}>
+                {todo.text}
+            </span>
+            <button onClick={()=>deleteTodo(index)}>削除する</button>
+        </li>
+    );
 }
