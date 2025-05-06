1e30260b39d5177e5c1f3481b67cb39980a5cbd7
kouheigit <ookawakouhei777@gmail.com>
2025-05-06 14:01:09 +0900

    con28

--- src/resources/js/division/TodoList.jsx
+++ src/resources/js/division/TodoList.jsx
@@ -5,7 +5,10 @@ export default function TodoList(){
     const { todos,toggleCheck,deleteTodo } = useTodos();
     return(
        <ul>
-           
+               {todos.map((todo,index)=>(
+                   <TodoItem>
+                   </TodoItem>
+               ))}
        </ul>
     );
 }
