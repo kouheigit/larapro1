2831fee253741ec52dc55afec797661f96273a8c
kouheigit <ookawakouhei777@gmail.com>
2025-05-06 15:47:06 +0900

    con31

--- src/resources/js/division/ListPage.jsx
+++ src/resources/js/division/ListPage.jsx
@@ -1,2 +1,11 @@
 import TodoItem from "./TodoItem";
-import TodoItem from "./TodoForm";
+import TodoForm from "./TodoForm";
+
+export default function ListPage(){
+    return(
+        <div>
+            <TodoItem/>
+            <TodoForm/>
+        </div>
+    );
+}
