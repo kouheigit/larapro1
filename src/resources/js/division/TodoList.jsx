b44eaa755d0612e746c889172e322fccae0c7f6f
kouheigit <ookawakouhei777@gmail.com>
2025-05-06 13:50:18 +0900

    con27

--- src/resources/js/division/TodoList.jsx
+++ src/resources/js/division/TodoList.jsx
@@ -1,1 +1,11 @@
 import{ useTodos } from '../contexts/TodoContext';
+import TodoItem from "./TodoItem";
+
+export default function TodoList(){
+    const { todos,toggleCheck,deleteTodo } = useTodos();
+    return(
+       <ul>
+           
+       </ul>
+    );
+}
