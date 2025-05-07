09e21b94f66f83f19077e5c1322a507ff455b601
kouheigit <ookawakouhei777@gmail.com>
2025-05-07 17:54:37 +0900

    con38

--- src/resources/js/practice/AppPractice.jsx
+++ src/resources/js/practice/AppPractice.jsx
@@ -2,6 +2,10 @@ import ReactDOM from 'react-dom'
 import React,{ useState } from 'react';
 
 function AppPractice(){
+    const[todos,setTodos] =([]);
+    const[inputs,setInputs] =('');
+
+    
     return (
         <div>
 
