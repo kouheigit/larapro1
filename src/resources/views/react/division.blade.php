918128ad8266ca65a96d41e95b747cb07b9c08dc
kouheigit <ookawakouhei777@gmail.com>
2025-05-07 14:49:41 +0900

    con36

--- src/resources/views/react/division.blade.php
+++ src/resources/views/react/division.blade.php
@@ -1,5 +1,5 @@
 <meta name="csrf-token" content="{{ csrf_token() }}">
 <link href="{{ mix('css/app.css') }}" rel="stylesheet" type="text/css">
 <h1>コンポーネント分割設計</h1>
-<div id="ListPages"></div>
+<div id="App1"></div>
 <script src="{{ mix('js/app.js') }}"></script>
