e14910b859b8911bb66f2302cefa49823d8ad020
kouheigit <ookawakouhei777@gmail.com>
2025-05-16 16:25:53 +0900

    dev2

--- src/resources/js/todoapi/Todo.js
+++ src/resources/js/todoapi/Todo.js
@@ -1,10 +1,18 @@
 import ReactDom from 'react-dom';
 import React,{ useState,useEffect } from 'react';
+import ReactDOM from "react-dom";
+import AdvanceTodo from "../components/AdovanceTodo";
 
 function Todo(){
     return(
         <div>
-
+            <h1>テスト</h1>
         </div>
     );
+}
+
+export default Todo;
+
+if (document.getElementById('Todo')) {
+    ReactDOM.render(<Todo />, document.getElementById('Todo'));
 }
