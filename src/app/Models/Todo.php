3ffcc13325124310ae97b239a9e5fcf0d8d8904f
kouheigit <ookawakouhei777@gmail.com>
2025-05-23 08:12:43 +0900

    dev60

--- src/app/Models/Todo.php
+++ src/app/Models/Todo.php
@@ -8,7 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Todo extends Model
 {
     use HasFactory;
-    protected $table = 'todo';
     protected $fillable = [
         'text',
         'done',
