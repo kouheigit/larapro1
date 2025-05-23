c1cb910cc914a15b8c5c017ec8b46c234e5b2044
kouheigit <ookawakouhei777@gmail.com>
2025-05-23 17:00:10 +0900

    dev64

--- src/resources/js/todoapi/hooks/useTodo.js
+++ src/resources/js/todoapi/hooks/useTodo.js
@@ -3,7 +3,18 @@ import React, { useState,useEffect,useReducer } from 'react';
 export function useTodo(){
     const [todo, dispatch] = useReducer(todoReducer, []);
     const [input, setInput] = useState('');
-    
+
+    useEffect(()=>{
+        fetch('http://localhost:8000/api/todos')
+            .then(res=>res.json())
+            .then(data=>{
+                data.forEach(todo=>{
+                    dispatch({type:'ADD',text: todo.text, done: todo.done });
+                })
+            })
+            .catch(err=>console.error('GET/api/todos 失敗:',err));
+        }, []);
+
     //addTodo
     const addTodo = () => {
         if (input.trim() === "") {
@@ -12,6 +23,25 @@ export function useTodo(){
         dispatch({ type: 'ADD',text: input });
         setInput('');
     }
+    /*
+    const addTodo = () => {
+  if (input.trim() === '') return;
+
+  fetch('http://localhost:8000/api/todos', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ text: input })
+  })
+    .then(res => res.json())
+    .then(newTodo => {
+      dispatch({ type: 'ADD', text: newTodo.text, done: newTodo.done });
+      setInput('');
+    })
+    .catch(err => console.error('POST /api/todos 失敗:', err));
+};
+     */
     //deleteTodo
     const deleteTodo = (deleteindex) =>{
         dispatch({type:'DELETE',index:deleteindex});
