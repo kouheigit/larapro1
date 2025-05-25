fdf4a0f339c7658042a063130ce7d360462c9d8d
kouheigit <ookawakouhei777@gmail.com>
2025-05-25 19:16:17 +0900

    dev77

--- src/routes/api.php
+++ src/routes/api.php
@@ -1,8 +1,10 @@
 <?php
 
 use App\Http\Controllers\ReactapiController;
+use App\Http\Controllers\TodoController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -21,3 +23,13 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::group(['middleware' => 'api'], function () {
     Route::match(['post', 'get'], 'Reactapi', [ReactapiController::class, 'insert']);
 });
+
+
+
+/*
+ * Route::get('/todos', [TodoController::class, 'index']);
+Route::post('/todos', [TodoController::class, 'store']);
+Route::delete('/todos/{id}', [TodoController::class, 'destroy']);
+Route::patch('/todos/{id}', [TodoController::class, 'update']);
+ */
+
