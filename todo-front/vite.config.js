f26c1114314fe2e9eb6e5a2658d847d568be7cc1
kouheigit <ookawakouhei777@gmail.com>
2025-05-31 13:58:41 +0900

    dev106

--- todo-front/vite.config.js
+++ todo-front/vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  test:{
+    //ここの部分を追加した
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/setupTests.js',
+  },
+});
+
