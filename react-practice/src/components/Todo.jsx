959bb287be817ca4e9efb678c938e33cb755b0a7
kouheigit <ookawakouhei777@gmail.com>
2025-06-07 14:03:35 +0900

    todo2

--- react-practice/src/components/Todo.jsx
+++ react-practice/src/components/Todo.jsx
@@ -2,9 +2,27 @@ import ReactDOM from'react-dom';
 import React,{ useState } from 'react';
 
 function Todo(){
-const[todos,settodos] =useState([]);
-const[inputs,setinputs] =useState();
+    const[todos,setTodos] =useState([]);
+    const[inputs,setInputs] =useState();
 
+    const addTodo = () => {
+        if(inputs.trim()=='') return;
+        setTodos([...todos,{ text: inputs, done:false }]);
+        setInputs('');
+    };
+
+ 
+
+    return(
+        <div>
+            <li>
+                <input type="text" value={input} onChange={(e)=>setInputs(e.target.value)}/>
+                <p>入力された値:{inputs}</p>
+                <button onClick={addTodo}>追加</button>
+            </li>
+        </div>
+    );
 
 }
+
 export default Todo;
