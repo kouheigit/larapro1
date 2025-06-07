b4163a2904eb667c38c2344147ca01634d0582c7
kouheigit <ookawakouhei777@gmail.com>
2025-06-07 13:14:59 +0900

    todo1

---
+++ react-practice/src/components/Todo.jsx
@@ -0,0 +1,10 @@
+import ReactDOM from'react-dom';
+import React,{ useState } from 'react';
+
+function Todo(){
+const[todos,settodos] =useState([]);
+const[inputs,setinputs] =useState();
+
+
+}
+export default Todo;
