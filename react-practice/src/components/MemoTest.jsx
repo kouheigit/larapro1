e5691b241c8a996442761edd9dc83e4db7b8de12
kouheigit <ookawakouhei777@gmail.com>
2025-06-10 20:57:17 +0900

    todo37

--- react-practice/src/components/MemoTest.jsx
+++ react-practice/src/components/MemoTest.jsx
@@ -7,14 +7,21 @@ function MemoTest(){
         { id:1,text:'買い物',done:false },
         { id:2,text:'勉強',done:true },
         { id:3,text:'運動',done:false}
-
     ]);
-   return(
+
+    /*
+    const filteredTodos = todos.filter((todo) => {
+        if (filter === 'done') return todo.done;
+        if (filter === 'undone') return !todo.done;
+        return true;
+    });*/
+
+    return(
        <div>
            <button onClick={()=>setFilter('all')}>全て</button>
            <button onClick={()=>setFilter('done')}>完了</button>
            <button onClick={()=>setFilter('undone')}>未完了</button>
-           
+
            <ul>{todos.map((todo)=>(
                <li key={todo.id}>{todo.text}</li>
            ))}
