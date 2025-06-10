7f11babdc594075d109197adcbe406d382c0bf02
kouheigit <ookawakouhei777@gmail.com>
2025-06-10 21:13:08 +0900

    todo39

--- react-practice/src/components/MemoTest.jsx
+++ react-practice/src/components/MemoTest.jsx
@@ -1,4 +1,4 @@
-import React,{ useState }  from'react';
+import React,{ useState,useMemo }  from'react';
 
 
 function MemoTest(){
@@ -8,17 +8,14 @@ function MemoTest(){
         { id:2,text:'勉強',done:true },
         { id:3,text:'運動',done:false}
     ]);
-    const filteredTodos = todos.filter((todo)=>{
-        if(filter ==='done') return todo.done;
-        if(filter==='undone') return !todo.done;
-        return true;
-    })
-    /*
-    const filteredTodos = todos.filter((todo) => {
-        if (filter === 'done') return todo.done;
-        if (filter === 'undone') return !todo.done;
-        return true;
-    });*/
+    const filteredTodos = useMemo(()=> {
+        todos.filter((todo) => {
+            if (filter === 'done') return todo.done;
+            if (filter === 'undone') return !todo.done;
+            return true;
+        }, [todos, filter]);
+    });
+
 
     return(
        <div>
@@ -26,7 +23,7 @@ function MemoTest(){
            <button onClick={()=>setFilter('done')}>完了</button>
            <button onClick={()=>setFilter('undone')}>未完了</button>
 
-           <ul>{todos.map((todo)=>(
+           <ul>{filteredTodos.map((todo)=>(
                <li key={todo.id}>{todo.text}</li>
            ))}
            </ul>
@@ -34,34 +31,3 @@ function MemoTest(){
    );
 }
 export default MemoTest;
-/*
-import React, { useState } from 'react';
-
-function TodoApp() {
-    const [filter, setFilter] = useState('all');
-    const [todos, setTodos] = useState([
-        { id: 1, text: '買い物', done: false },
-        { id: 2, text: '勉強', done: true },
-        { id: 3, text: '運動', done: false }
-    ]);
-
-    const filteredTodos = todos.filter((todo) => {
-        if (filter === 'done') return todo.done;
-        if (filter === 'undone') return !todo.done;
-        return true;
-    });
-
-    return (
-        <div>
-            <button onClick={() => setFilter('all')}>すべて</button>
-            <button onClick={() => setFilter('done')}>完了</button>
-            <button onClick={() => setFilter('undone')}>未完了</button>
-
-            <ul>
-                {filteredTodos.map((todo) => (
-                    <li key={todo.id}>{todo.text}</li>
-                ))}
-            </ul>
-        </div>
-    );
-}*/
