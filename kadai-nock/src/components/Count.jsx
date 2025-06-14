c0b1793472e0959161475fab08ac89c4914558e9
kouheigit <ookawakouhei777@gmail.com>
2025-06-14 17:59:26 +0900

    kadai2

---
+++ kadai-nock/src/components/Count.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+function Count(){
+    const[count,setCount] = useState(0);
+    const addClick = () =>{
+        setCount(count +1 );
+    }
+    const 
+    return(
+        <div>
+            <h1>Count処理</h1>
+        </div>
+    );
+}
---
+++ kadai-nock/src/pages/HelloPage.jsx
@@ -0,0 +1,11 @@
+import Hello from "../components/Hello";
+
+function HelloPage(){
+    return(
+        <div>
+            <Hello />
+        </div>
+    );
+}
+export default HelloPage;
+
