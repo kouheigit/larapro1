47b2789e4045686ccf8636eb74297d9750a0d18f
kouheigit <ookawakouhei777@gmail.com>
2025-06-14 18:04:23 +0900

    kadai3

--- kadai-nock/src/components/Count.jsx
+++ kadai-nock/src/components/Count.jsx
@@ -2,13 +2,20 @@ import { useState } from 'react'
 
 function Count(){
     const[count,setCount] = useState(0);
-    const addClick = () =>{
+    const addClick = () => {
         setCount(count +1 );
     }
-    const 
+
+    const subtractClick = () => {
+        setCount(count-1);
+    }
     return(
         <div>
+            <p>{count}</p>
+            <button onClick={addClick}></button>
+            <button onClick={subtractClick}></button>
             <h1>Count処理</h1>
         </div>
     );
 }
+export default Count;
