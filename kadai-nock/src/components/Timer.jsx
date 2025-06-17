2907c51d86a33e1d1a4e4acbfe83d1a19f514857
kouheigit <ookawakouhei777@gmail.com>
2025-06-17 19:52:20 +0900

    kadai21

---
+++ kadai-nock/src/components/Timer.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { useTimer } from 'react-timer-hook';
+
+function Timer() {
+    const {
+        second,
+        minutes,
+        hours,
+        start,
+        pause,
+        resume,
+        restart,
+    }=useTimer({autoStart:true});
+
+    return(
+        <div>
+            <div>
+                <span>{hours}</span>:<span>{minutes}</span>:<span>{seconds}</span>
+            </div>
+            <button onClick={start}>スタート</button>
+            <button onClick={pause}>一時停止</button>
+            <button onClick={resume}>再スタート</button>
+        </div>
+    );
+}
+export default Timer;
