8b512d3600264a1bd46f38f33063f5836875b78e
kouheigit <ookawakouhei777@gmail.com>
2025-06-24 18:24:41 +0900

    kadai44

--- kadai-nock/src/components/Timer.jsx
+++ kadai-nock/src/components/Timer.jsx
@@ -2,11 +2,12 @@ import React, { useState, useEffect } from 'react';
 
 const Timer = () => {
     const [count, setCount] = useState(0);
-    //const [isRunning, setIsRunning] = useState(false);
+    const [isRunning, setIsRunning] = useState(false);
 
     useEffect(() => {
         // カウントが0以下なら何もしない（止める）
-        if (count <= 0) return;
+        if (count <= 0||!isRunning) return;
+
 
         // 1秒後に -1 するタイマーをセット
         const timer = setTimeout(() => {
@@ -15,15 +16,28 @@ const Timer = () => {
 
         // 次にこの useEffect が動くとき、前のタイマーを止める
         return () => clearTimeout(timer);
-    }, [count]); // ← count が変わるたびに動く
+    }, [count,isRunning]); // ← count が変わるたびに動く
 
     const addSeconds = () => {
         setCount(prev => prev + 10);
+        //isRunning(false);
     };
+    const timerStart = () => {
+        if(count > 0){
+            setIsRunning(true);
+        }
+    };
+    const timerStop = () =>{
+        setIsRunning(false);
+    }
+
+
 
     return (
         <div>
             <button onClick={addSeconds}>10秒</button>
+            <button onClick={timerStart}>スタート</button>
+            <button onClick={timerStop}>ストップ</button>
             <h1>タイマー: {count} 秒</h1>
         </div>
     );
