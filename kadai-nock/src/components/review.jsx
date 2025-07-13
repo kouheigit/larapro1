b9ef39fd4f6c9669e36ebe800ea6bc50cbfeaa45
kouheigit <ookawakouhei777@gmail.com>
2025-07-13 10:15:19 +0900

    review2

--- kadai-nock/src/components/review.jsx
+++ kadai-nock/src/components/review.jsx
@@ -1,6 +1,12 @@
 import ReactDOM from'react-dom';
 import React,{ useState } from'react';
 function Todo(){
+    const[todos,setTodos] = useState([]);
+    const[inputs,setInuputs] = useState('');
+
+    const addTodo = () =>{
+        
+    }
 }
 return(
     <div>
