fcabccceeff26368e13a91232189d792f9f75ff4
kouheigit <ookawakouhei777@gmail.com>
2025-07-13 10:34:58 +0900

    review4

--- kadai-nock/src/components/review.jsx
+++ kadai-nock/src/components/review.jsx
@@ -5,6 +5,14 @@ function Todo(){
     const[inputs,setInuputs] = useState('');
 
     const addTodo = () =>{
+        if(inputs.trim==='')return;
+        setTodos([...todos,{text:input, done:false}]);
+        setInuputs('');
+        /*
+         if(inputs.trim()==='') return;
+        setTodos([...todos,{ text: inputs, done:false }]);
+        setInputs('');
+         */
 
     }
     const deleteTodo = () =>{
@@ -13,6 +21,23 @@ function Todo(){
 }
 return(
     <div>
+        <ul>
+            <input type="text" value={inputs} onChange={(e)=>setInputs(e.target.value)} placeholder="タスクを入力"/>
+            <p>入力された値]{inputs}</p>
+            
 
+
+            {filterTodos.map((todo,index)=>{
+                return(
+                    <li key="index">
+                        <input type="checkbox" checked={todo.done} onChange={()=>setToggle(index)} />
+                        <span style={{ textDecoration: todo.done ? 'line-through' : 'none' }}>
+                                    {todo.text}
+                                </span>
+                        <button onClick={()=>deleteTodo(index)}>削除</button>
+                    </li>
+                );
+            })}
+        </ul>
     </div>
 );
