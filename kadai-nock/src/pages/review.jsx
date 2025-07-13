d93da7df38658dbb99969a42323d7e77a313cf38
kouheigit <ookawakouhei777@gmail.com>
2025-07-13 12:32:37 +0900

    review11

--- kadai-nock/src/pages/review.jsx
+++ kadai-nock/src/pages/review.jsx
@@ -1,12 +1,12 @@
 import ReactDOM from'react-dom';
 import React,{ useState } from'react';
-function Todo(){
-    const[todos,setTodos] = useState([]);
-    const[inputs,setInuputs] = useState('');
+function Todo() {
+    const [todos, setTodos] = useState([]);
+    const [inputs, setInuputs] = useState('');
 
-    const addTodo = () =>{
-        if(inputs.trim==='')return;
-        setTodos([...todos,{text:input, done:false}]);
+    const addTodo = () => {
+        if (inputs.trim === '') return;
+        setTodos([...todos, {text: input, done: false}]);
         setInuputs('');
         /*
          if(inputs.trim()==='') return;
@@ -15,25 +15,25 @@ function Todo(){
          */
 
     }
+    /*
     const deleteTodo = () =>{
 
-    }
+    }*/
+
+    return (
+        <div>
+            <ul>
+                <input type="text" value={inputs} onChange={(e) => setInuputs(e.target.value)} placeholder="タスクを入力"/>
+                <p>入力された値]{inputs}</p>
+                <button onClick={addTodo}>追加</button>
+                {todos.map((todo, index) => (
+                    <li key={index}>
+                        {todo.text}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
 }
-return(
-    <div>
-        <ul>
-            <input type="text" value={inputs} onChange={(e)=>setInuputs(e.target.value)} placeholder="タスクを入力"/>
-            <p>入力された値]{inputs}</p>
-            <button onClick={addTodo}>追加</button>
-            {todos.map((todo, index) => (
-                <li key={index}>
-                    {todo.text}
-                    <button onClick={() => deleteTodo(index)}>削除</button>
-                </li>
-            ))}
-        </ul>
-    </div>
-);
 
-
-export default Todo1;
+export default Todo;
