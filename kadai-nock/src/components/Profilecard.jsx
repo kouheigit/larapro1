f482f6163da7377378bb737a68ad1b12cfde21d9
kouheigit <ookawakouhei777@gmail.com>
2025-07-14 21:48:41 +0900

    profile

--- kadai-nock/src/components/Profilecard.jsx
+++ kadai-nock/src/components/Profilecard.jsx
@@ -9,6 +9,7 @@ function Profilecard(){
 
     return(
         <div>
+            <h2>プロフィール登録</h2>
             <input type="text" value={name} onChange={(e)=>setName(e.target.value)} placeholder="タスクを入力"/>
             <input type="text" value={hobby} onChange={(e)=>setHobby(e.target.value)} placeholder="タスクを入力"/>
             <input type="text" value{comment} onChange={(e)=>setComment(e.target.value)} placeholder="タスクを入力"/>
