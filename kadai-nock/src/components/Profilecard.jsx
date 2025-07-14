ffdcf10d5e8f6d1b09a328681cc01292caf6792c
kouheigit <ookawakouhei777@gmail.com>
2025-07-14 20:59:05 +0900

    review20

---
+++ kadai-nock/src/components/Profilecard.jsx
@@ -0,0 +1,16 @@
+import React,{ useState } from 'react';
+import Todo from "../pages/review.jsx";
+
+function Profilecard(){
+    const [name,setName] = useState('');
+    const [hobby,setHobby] = useState('');
+    const[comment,setComment] = useState('');
+    const[show,setShow] = useState(false);
+    
+    return(
+        <div>
+
+        </div>
+    );
+}
+export default Profilecard;
