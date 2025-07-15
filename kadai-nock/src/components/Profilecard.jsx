362f32a040e06128372ed34ca3223e9a8213ff1a
kouheigit <ookawakouhei777@gmail.com>
2025-07-15 10:11:51 +0900

    profile7

--- kadai-nock/src/components/Profilecard.jsx
+++ kadai-nock/src/components/Profilecard.jsx
@@ -12,6 +12,10 @@ function Profilecard(){
         if(name.trim()===''||hobby.trim()===''||comment.trim()==='')return;
         const newProfile = { name,hobby,comment };
         setProfiles([...profiles,newProfile]);
+
+        setName('');
+        setHobby('');
+        setComment('');
     }
 
     return(
@@ -20,6 +24,19 @@ function Profilecard(){
             <input type="text" value={name} onChange={(e)=>setName(e.target.value)} placeholder="タスクを入力"/>
             <input type="text" value={hobby} onChange={(e)=>setHobby(e.target.value)} placeholder="タスクを入力"/>
             <input type="text" value{comment} onChange={(e)=>setComment(e.target.value)} placeholder="タスクを入力"/>
+            {profiles.map(()=>(
+                <div>
+                    <h1>テスト</h1>
+                </div>
+            ))}
+            {todos.map((todo, index) => (
+                <li key={index}>
+                    <input type="checkbox" checked={todo.done} onChange={()=>toggleCheck(index)}/>
+                    <span style={{ textDecoration: todo.done ? 'line-through' : 'none' }}>
+                        {todo.text}
+                        <button onClick={()=>deleteTodo(index)}>削除</button></span>
+                </li>
+            ))}
         </div>
     );
 }
