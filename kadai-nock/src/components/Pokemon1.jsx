d653d53bda50fb562046860308b0566c88bf38ff
kouheigit <ookawakouhei777@gmail.com>
2025-07-25 18:49:34 +0900

    pokemon31

--- kadai-nock/src/components/Pokemon1.jsx
+++ kadai-nock/src/components/Pokemon1.jsx
@@ -3,7 +3,10 @@ import React,{ useEffect,useState }from 'react';
 function Pokemon1(){
     const[pokemon,setPokemon] = useState(null);
     useEffect(()=>{
-
+        fetch('https://pokeapi.co/api/v2/pokemon/pikachu')
+            .then(res => res.json())
+            .then(data => setPokemon(data))
+            .catch(err => console.error('取得失敗',err));
     },[]);
     return(
         <div>
@@ -12,5 +15,7 @@ function Pokemon1(){
     );
 }
 export default Pokemon1;
+
+
 
 
