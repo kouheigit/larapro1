55de625ffb6a529576531d3a95ec1be49bd3ed33
kouheigit <ookawakouhei777@gmail.com>
2025-07-27 23:23:35 +0900

    pokemon43

--- kadai-nock/src/components/Pokemon2.jsx
+++ kadai-nock/src/components/Pokemon2.jsx
@@ -1,13 +1,18 @@
 import React,{ useEffect,useState }from'react';
+import Pokemon1 from "./Pokemon1.jsx";
 
 function Pokemon2() {
     //  const[pokemon,setPokemon] = useState(null);
     const[pokemon,setPokemon] = useState(null);
     useEffect(()=>{
         fetch('https://pokeapi.co/api/v2/pokemon/pikachu')
+            .then(res=>res.json())
+            .then(data=>setPokemon(data))
+            .catch(err => console.error('取得失敗',err));
     },[]);
-   
 
+    if (!pokemon) return <p>読み込み中...</p>;
+    
    return(
        <div>
 
@@ -15,3 +20,4 @@ function Pokemon2() {
    );
 
 }
+export default Pokemon1;
