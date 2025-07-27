dfe17ea3ff45333018d80dc6594c49ad7cf3d2a2
kouheigit <ookawakouhei777@gmail.com>
2025-07-27 23:22:09 +0900

    pokemon42!

--- kadai-nock/src/components/Pokemon2.jsx
+++ kadai-nock/src/components/Pokemon2.jsx
@@ -4,9 +4,9 @@ function Pokemon2() {
     //  const[pokemon,setPokemon] = useState(null);
     const[pokemon,setPokemon] = useState(null);
     useEffect(()=>{
-
+        fetch('https://pokeapi.co/api/v2/pokemon/pikachu')
     },[]);
- 
+   
 
    return(
        <div>
