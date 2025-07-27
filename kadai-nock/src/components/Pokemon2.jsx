5b313bb4b05c11ec4d9a28f78d0e9a43dbf278e3
kouheigit <ookawakouhei777@gmail.com>
2025-07-27 23:18:27 +0900

    pokemon40

--- kadai-nock/src/components/Pokemon2.jsx
+++ kadai-nock/src/components/Pokemon2.jsx
@@ -1,7 +1,8 @@
 import React,{ useEffect,useState }from'react';
 
 function Pokemon2() {
-    
+    //  const[pokemon,setPokemon] = useState(null);
+    const[pokemon,setPokemon] = useState(null);
    return(
        <div>
 
