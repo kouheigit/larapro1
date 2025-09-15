722315b9a542a3dc5ecab138f34c80fb5cbbdb30
kouheigit <ookawakouhei777@gmail.com>
2025-09-15 10:56:22 +0900

    todo1 2

--- kadai-nock/src/components/Todo1.jsx
+++ kadai-nock/src/components/Todo1.jsx
@@ -5,7 +5,9 @@ function Todo1(){
     const[inputs,setInputs] = useState('');
 
     const addTodo = () =>{
-        
+        if(inputs.trim==='') return;
+        setTodos(...[todos,{text:inputs,done:false}]);
+        setInputs('');
     }
 
     return(
