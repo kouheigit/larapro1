af2c78eac3672eeecc9f6ff604f22f1cba94230d
kouheigit <ookawakouhei777@gmail.com>
2025-09-15 10:40:51 +0900

    todo1

---
+++ kadai-nock/src/components/Todo1.jsx
@@ -0,0 +1,14 @@
+import React,{ useState } from 'react';
+
+function Todo1(){
+    const[todos,setTodos] = useState([]);
+    const[inputs,setInputs] = useState('');
+    
+
+
+    return(
+        <div>
+
+        </div>
+    );
+}
