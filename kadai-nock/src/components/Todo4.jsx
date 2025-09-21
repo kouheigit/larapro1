7b819b02204a85dafd819135f144340fac8c1ae8
kouheigit <ookawakouhei777@gmail.com>
2025-09-21 20:26:37 +0900

    todo5.jsx2

--- kadai-nock/src/components/Todo4.jsx
+++ kadai-nock/src/components/Todo4.jsx
@@ -3,6 +3,7 @@ import React,{ useState } from 'react';
 function Todo4(){
     const[todos,setTodos] = useState([]);
     const[input,setInputs] = useState('');
+
     const addTodo= () => {
         if(inputs.trim()==='')return;
         setTodos([...todos,{text,inputs,done:false}]);
@@ -11,6 +12,9 @@ function Todo4(){
 
     return(
         <div>
+            <input type="text" value={inputs} onChange={(e)=>setInputs(e.target.value)}/>
+            <button onClick={addTodo}>追加</button>
+            
 
         </div>
     )
