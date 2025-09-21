4b64c83f3c625bc73dd0fc77c7ee4963a0a96739
kouheigit <ookawakouhei777@gmail.com>
2025-09-21 20:19:12 +0900

    Todo4.jsx

---
+++ kadai-nock/src/components/Todo4.jsx
@@ -0,0 +1,17 @@
+import React,{ useState } from 'react';
+
+function Todo4(){
+    const[todos,setTodos] = useState([]);
+    const[input,setInputs] = useState('');
+    const addTodo= () => {
+        if(inputs.trim()==='')return;
+        setTodos([...todos,{text,inputs,done:false}]);
+        setInputs('');
+    }
+    
+    return(
+        <div>
+
+        </div>
+    )
+}
